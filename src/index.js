f6ad4ce5dd795e29f72634cceba5ad71f1ec665b
ajg7 <ajgebara@gmail.com>
2020-09-19 22:31:12 -0500

    started making the store of the app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(
+  //Create your main reducers, then combine them in index.js/reducers, and then import them in this file as allReducer
+  // allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+
+
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
