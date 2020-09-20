db22444c902d7bd3b933492f7592c3c1f0905e1c
ajg7 <ajgebara@gmail.com>
2020-09-19 21:55:18 -0500

    added pokemon and pokemon list components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import axios from "axios";
+import PokemonList from "./components/PokemonList";
 
 function App() {
+
+
   return (
     <div className="App">
       <h2>Gotta Catch 'em All!</h2>
+      <PokemonList />
     </div>
   );
 }
---
+++ src/components/PokemonList.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
+const PokemonList = props => {
+
+    
+
+    return(
+        <>
+            
+        </>
+    )
+}
+
+export default PokemonList;
