c5a224a2d5f26125517f88deb2f1b234c64cfd4c
ajg7 <ajgebara@gmail.com>
2020-09-19 22:23:21 -0500

    made an index.js file for the reducer folder

---
+++ src/store/reducers/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux";
+
+/*
+    here I can put all my reducers in one area with combineReducers
+
+    const allReducers = combineReducers({
+
+        example: exampleReducer,
+        anotherExample: anotherExampleReducer
+    })
+
+
+*/
