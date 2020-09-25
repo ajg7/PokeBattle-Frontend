b5f46fbd4313a067ab0130ef27e3501453d0d34a
ajg7 <ajgebara@gmail.com>
2020-09-25 18:47:46 -0500

    added title and pokeball image for the pokemon team

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import PokemonList from "./components/PokemonList";
+import "./css/styles.css";
+import PokeballStorage from "./assets/PokemonStorage.png"
 
 const App = props => {
 
   return (
-    <div className="App">
-      <PokemonList />
-    </div>
+    <>
+      <header>
+        <h1>Gotta Catch 'em All!</h1>
+        <img src={PokeballStorage} alt="storage for your pokemon team" />
+      </header>
+      <div className="pokemon-app">
+        <PokemonList />
+      </div>
+    </>
   );
 }
 
