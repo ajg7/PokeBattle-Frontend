5bfeb553595a921ad81ef272dac9af0bed9c25b4
ajg7 <ajgebara@gmail.com>
2020-09-24 21:46:58 -0500

    made some cards, however their is a bug where empty cards are created after Mew

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useState, useEffect } from "react";
 import "../css/reset.css";
 import "../css/styles.css";
+import { PokemonCardStyledComponent } from "../styled_components/PokemonCardStyledComponent";
 
 const Pokemon = props => {
     const { name, url } = props;
@@ -26,14 +27,16 @@ const Pokemon = props => {
 
 
     return(
-        <div className="pokemon-cards">
-            <div className="pokemon-card">
-                {id <= 151 ? <img src={img} alt={name} /> : null}
-                {id <= 151 ? <h2>{name}</h2> : null}
-                {id <= 151 ? <h3>{id}</h3> : null}
-                {id <= 151 ? <h3>{type}</h3> : null}
+        <PokemonCardStyledComponent>
+            <div className="pokemon-cards">
+                <div className="pokemon-card">
+                    {id <= 151 ? <img src={img} alt={name} /> : null}
+                    {id <= 151 ? <h2>{name}</h2> : null}
+                    {id <= 151 ? <h3>{id}</h3> : null}
+                    {id <= 151 ? <h3>{type}</h3> : null}
+                </div>
             </div>
-        </div>
+        </PokemonCardStyledComponent>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reducer from "./store/reducers/reducer"
 import thunk from "redux-thunk";
 import logger from "redux-logger";
+import { ThemeProvider } from "styled-components";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { PokemonCardStyledComponent as theme } from "./styled_components/PokemonCardStyledComponent"
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
