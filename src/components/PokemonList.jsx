9bef0ee982a41fa8a361576f44ea51e5ffb11793
ajg7 <ajgebara@gmail.com>
2020-10-25 21:03:59 -0500

    pokemon cards show up, and also show up to only authorized people

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -9,19 +9,6 @@ import "../css/styles.css";
 
 const PokemonList = props => {
     // const { pokemon, fetchPokemonList } = props;
-    // const [currentList, setCurrentList] = useState("https://pokemon-server-ajg7.herokuapp.com/pokemon");
-    // const [name, setName] = useState("");
-    // const [type1, setType1] = useState("");
-    // const [type2, setType2] = useState("");
-    // const [imgURL, setImgURL] = useState("");
-    // const [number, setNumber] = useState(0);
-    // const [height, setHeight] = useState(0);
-    // const [weight, setWeight] = useState(0);
-    // const [entry, setEntry] = useState("");
-    // const [habitat, setHabitat] = useState("");
-    // const [legendary, setLegendary] = useState(false);
-    // const [mythical, setMythical] = useState(false);
-    // const [ancient, setAncient] = useState(false);
     const [pokemonData, setPokemonData] = useState([])
     
     
@@ -32,30 +19,12 @@ const PokemonList = props => {
 
     useEffect(() => {
         axiosWithAuth().get("https://pokemon-server-ajg7.herokuapp.com/pokemon")
-            .then(response => {
-                console.log(response.data)
-                setPokemonData(response.data)
-            //     const data = response.data;
-            //     setName(data.name)
-            // setType1(data.type1)
-            // setType2(data.type2)
-            // setImgURL(data.imgURL)
-            // setNumber(data.number)
-            // setHeight(data.height)
-            // setWeight(data.weight)
-            // setWeight(data.entry)
-            // setHabitat(data.habitat)
-            // setLegendary(data.legendary)
-            // setMythical(data.mythical)
-            // setAncient(data.ancient)
-            //     console.log(data)
-            })
+            .then(response => setPokemonData(response.data))
     }, [])
 
     return(
         <>
             {pokemonData.map(pokemon => {
-                console.log(pokemon)
                 return (
                     <Pokemon 
                     name={pokemon.name}
