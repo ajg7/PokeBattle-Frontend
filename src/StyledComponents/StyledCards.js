baddcc9c3e1ca743501b3a558452095b2a5db611
ajg7 <ajgebara@gmail.com>
2020-11-09 21:43:57 -0600

    edited the padding top for the cards

--- src/StyledComponents/StyledCards.js
+++ src/StyledComponents/StyledCards.js
@@ -6,6 +6,7 @@ export const StyledCards = styled.div`
     justify-content: space-between;
     text-transform: ${({ theme }) => theme.cap};
     padding: 50px;
+    padding-top: 250px;
 
     .card {
         background-color: transparent;
