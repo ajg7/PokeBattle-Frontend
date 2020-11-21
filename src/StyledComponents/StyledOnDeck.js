a43455bfb1779098f138e3307b0f7d2d70aef5ce
ajg7 <ajgebara@gmail.com>
2020-11-20 22:06:34 -0600

    OnDeck cards are fully rendered, if nickname is not null, then it will render it instead of name

---
+++ src/StyledComponents/StyledOnDeck.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledOnDeck = styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-evenly;
+    text-transform: capitalize;
+    text-align: center;
+    .cards {
+        border: 1px red solid;
+    }    
+
+`
--- src/components/OnDeck.jsx
+++ src/components/OnDeck.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import {connect} from "react-redux";
 import { fetchPokemonTeam } from "../store/actions/actions";
+import { StyledOnDeck } from "../StyledComponents/StyledOnDeck";
 
 const OnDeck = props => {
     const { teamData, teamId, fetchPokemonTeam } = props;
@@ -20,19 +21,22 @@ const OnDeck = props => {
     return (
         <>
             <h2>Ready to Battle?</h2>
-            <div>
+            <StyledOnDeck>
                 {teamData.map(member => {
                     return (
-                        <div>
-                        <h3>{member.name}</h3>
-                        <h3>{member.type1}</h3>
-                        <h3>{member.type2}</h3>
-                        <img src={member.imgURL} alt={member.name} />
-                        </div>
+                            <div className="cards">
+                                <div>
+                                    <img src={member.imgURL} alt={member.name} />
+                                </div> 
+                                <h3>{member.pokemon_Id}</h3>
+                                <h2>{member.nickname ? member.nickname : member.name}</h2>
+                                <h3>{member.type1}</h3>
+                                <h3>{member.type2}</h3>
+                            </div>
                     )
                 })}
-                <button onClick={goBackHandler}>Go Back</button>
-            </div>
+            </StyledOnDeck>
+            <button onClick={goBackHandler}>Go Back</button>
         </>
     )
 }
