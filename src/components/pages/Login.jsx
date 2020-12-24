11dd7a48068e2fdb25650c382fdfa2c0b3d20541
ajg7 <ajgebara@gmail.com>
2020-12-23 18:17:48 -0600

    refactored login and signup

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,43 +1,7 @@
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
-import axios from "axios";
-import { FormValues, initialFormValues } from "../../classes/FormValuesClass";
-import { Button, AuthForm, FormLabels } from "../common";
-import { connect } from "react-redux";
-import { fetchTeamId } from "../../store/actions/actions";
+import React from "react";
+import { AuthForm } from "../common";
 
 const Login = props => {
-
-    const { fetchTeamId, teamId } = props;
-    /*
-        1. Make Form Values (make a separate file for class, so I can import into Login Component)
-        2. Essentially, login should be just like Signup, except token should be stored in localStorage
-        3. Post login form values to the login endpoint
-        4. Add yup form Validation or React Form Validation that Max talked about
-    */
-
-    const history = useHistory();
-    const [formValues, setFormValues] = useState(initialFormValues);
-
-    const loginUser = event => {
-        event.preventDefault();
-        const user = new FormValues(formValues.email.trim(), formValues.password.trim(), false);
-        axios.post("http://localhost:7000/users/login", user)
-                .then(response => {
-                    console.log(response)
-                    const token = response.data.token;
-                    const userId = response.data.userId;
-                    localStorage.setItem("token", token);
-                    fetchTeamId(userId);
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-                .finally(() => {
-                    setFormValues(initialFormValues)
-                    history.push("/pokemon_list")
-                })
-    }  
 
     return(
         <div>
@@ -49,11 +13,4 @@ const Login = props => {
 }
 
 
-const mapStateToProps = state => {
-    return {
-        teamId: state.teamId
-    }
-}
-
-
-export default connect(mapStateToProps, { fetchTeamId })(Login);
+export default Login;
--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -1,44 +1,8 @@
-import axios from "axios";
-import React, { useState } from "react";
-import { fetchTeamId, makeTeam } from "../../store/actions/actions";
-import { connect } from "react-redux";
-import { useHistory } from "react-router-dom";
-import { FormValues, initialFormValues } from "../../classes/FormValuesClass";
-import { Button, AuthForm } from "../common";
+import React from "react";
+import { AuthForm } from "../common";
 
 
 const Signup = props => {
-    const { makeTeam } = props; 
-    const history = useHistory();
-    const [formValues, setFormValues] = useState(initialFormValues);
-
-    const inputChange = (key, value) => {
-        setFormValues({
-            ...formValues,
-            [key]: value
-        })
-    }
-
-    const changeHandler = event => {
-        const { name, value } = event.target
-        inputChange(name, value)
-    }
-
-    const addNewUser = event => {
-        event.preventDefault();
-        const newUser = new FormValues(formValues.email.trim(), formValues.password.trim(), false);
-        axios.post("http://localhost:7000/users/signup", newUser)
-            .then(response => {
-                setFormValues(initialFormValues);
-                const token = response.data.token;
-                localStorage.setItem("token", token);
-                makeTeam();
-                history.push("/pokemon_list");
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }
 
     return(
         <div>
@@ -49,4 +13,4 @@ const Signup = props => {
     )
 }
 
-export default connect(null, {makeTeam})(Signup);
+export default Signup;
