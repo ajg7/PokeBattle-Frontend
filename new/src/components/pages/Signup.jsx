8927ed408bb904cc0d4cfe14a21e0651572d6d9a
ajg7 <ajgebara@gmail.com>
2021-01-17 16:21:37 -0600

    made loading page and error page

--- new/src/components/pages/Signup.jsx
+++ new/src/components/pages/Signup.jsx
@@ -3,18 +3,18 @@ import React from "react";
 const Signup = () => {
 	return (
 		<div>
-            <header>
-            <h1>PokeBattle</h1>
-            </header>
-            <section>
-                <h2>Sign Up</h2>
-                <form>
-                    <label>Email</label>
-                    <input />
-                    <label>Password</label>
-                    <input />
-                </form>
-            </section>
+			<header>
+				<h1>PokeBattle</h1>
+			</header>
+			<section>
+				<h2>Sign Up</h2>
+				<form>
+					<label>Email</label>
+					<input />
+					<label>Password</label>
+					<input />
+				</form>
+			</section>
 			<footer>
 				<p>
 					Already have an account? <a href="/login">Log In</a>
--- new/src/components/pages/index.js
+++ new/src/components/pages/index.js
@@ -1,5 +1,7 @@
 import LandingPage from "./LandingPage";
 import Login from "./Login";
 import Signup from "./Signup";
+import Loading from "./Loading";
+import ErrorPage from "./ErrorPage";
 
-export { LandingPage, Login, Signup };
+export { LandingPage, Login, Signup, Loading, ErrorPage };
