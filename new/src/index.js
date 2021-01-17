72917bac453a17447a075acdcfb4b1b3174ad29c
ajg7 <ajgebara@gmail.com>
2021-01-17 17:39:30 -0600

    set up the store

--- new/src/index.js
+++ new/src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import store from "./store";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-	<Router>
-		<ThemeProvider theme={theme}>
-			<App />
-		</ThemeProvider>
-	</Router>,
+	<Provider store={store}>
+		<Router>
+			<ThemeProvider theme={theme}>
+				<App />
+			</ThemeProvider>
+		</Router>
+	</Provider>,
 	document.getElementById("root")
 );
