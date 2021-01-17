f048fa4a561fa22b6d4a0d03f7db42f6dded3da2
ajg7 <ajgebara@gmail.com>
2021-01-17 15:36:04 -0600

    made rough skeleton of the landing page

--- new/src/components/pages/LandingPage.jsx
+++ new/src/components/pages/LandingPage.jsx
@@ -2,8 +2,22 @@ import React from "react";
 
 const LandingPage = () => {
 	return (
-		<div>
-			<h3>Landing Page</h3>
+		<div className="landing-page">
+			<header>
+				<h1>PokeBattle</h1>
+				<nav>
+					<button>Sign Up</button>
+					<button>Log In</button>
+				</nav>
+			</header>
+			<section>
+				<img src={""} alt="pokemon of the day" />
+			</section>
+			<footer>
+				<h3>Created By: A.J. Gebara</h3>
+				<h3>Special Thanks to PokeApi for the Data</h3>
+				<h4>Check them out at <a href="https://pokeapi.co/">https://pokeapi.co/</a></h4>
+			</footer>
 		</div>
 	);
 };
