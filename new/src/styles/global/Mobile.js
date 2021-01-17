2c1fdf7f20de72e2d276f54dac8692aebbef5772
ajg7 <ajgebara@gmail.com>
2021-01-17 15:29:41 -0600

    added mobile widths

--- new/src/styles/global/Mobile.js
+++ new/src/styles/global/Mobile.js
@@ -1,17 +1,12 @@
 import { createGlobalStyle } from "styled-components";
-import pixelToViewportWidth from "../../utils/pixelToViewportWidth";
 
 export const Mobile = createGlobalStyle`
     :root {
-        font-size: ${pixelToViewportWidth(24)};
-
-        @media (max-width: 768px) {
-            font-size: ${pixelToViewportWidth(18)};
-        }
-
-        @media (min-width: 1024px) {
-            font-size: ${pixelToViewportWidth(16)};
-        }
+        /*
+            Sizes to keep in mind:
+            640, 768, 1080, 667, 900, 800
+        
+        */
     }
 
 `;
