6a03f2b0bdb583d3b4388e3abb070d44f1203958
ajg7 <ajgebara@gmail.com>
2021-01-19 09:30:30 -0600

    auth form can be used for both login and signup

--- new/src/components/common/AuthForm.jsx
+++ new/src/components/common/AuthForm.jsx
@@ -1,1 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
+import PropTypes from "prop-types";
+
+const AuthForm = props => {
+	const emailRef = useRef();
+	const passwordRef = useRef();
+	const { formType } = props;
+
+	console.log(emailRef);
+
+	const submitHandler = async event => {
+		event.preventDefault();
+		const email = emailRef.current.value;
+		const password = passwordRef.current.value;
+        const user = { email: email.trim(), password: password.trim() };
+		try {
+			if (formType === "Login") console.log(user, "login");
+			if (formType === "Signup") console.log(user, "signup");
+        } 
+        catch (error) {
+			console.log(error);
+        } 
+	};
+
+	return (
+		<div>
+			<form onSubmit={submitHandler}>
+				<label>
+					Email:
+					<input placeholder="Enter Email" type="text" ref={emailRef} />
+				</label>
+				<label>
+					Password:
+					<input placeholder="Enter Password" type="password" ref={passwordRef} />
+                </label>
+                <button>Submit</button>
+			</form>
+		</div>
+	);
+};
+
+AuthForm.propTypes = {
+	formType: PropTypes.string,
+};
+
+export default AuthForm;
