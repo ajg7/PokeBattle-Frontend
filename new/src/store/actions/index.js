7b242c746ee73e1a2cb3c808aa6ac9437cad92c6
ajg7 <ajgebara@gmail.com>
2021-01-23 23:13:30 -0600

    added battle action and battle reducer files

--- new/src/store/actions/index.js
+++ new/src/store/actions/index.js
@@ -1,5 +1,6 @@
 import * as pokemon from "./pokemonActions";
 import * as teams from "./teamActions";
 import * as teamMembers from "./teamMembersActions";
+import * as battle from "./battleActions";
 
-export { pokemon, teams, teamMembers };
+export { pokemon, teams, teamMembers, battle };
--- new/src/store/reducers/index.js
+++ new/src/store/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import { reducer as pokemon } from "./pokemonReducer";
 import { reducer as teams } from "./teamsReducer";
 import { reducer as teamMembers } from "./teamMembersReducer";
+import { reducer as battle } from "./battleReducer";
 
 export default combineReducers({
 	pokemon,
 	teams,
 	teamMembers,
+	battle,
 });
