1fe1200d548b2da1d2960d84c52c90bd770bf11c
ajg7 <ajgebara@gmail.com>
2021-01-24 12:41:49 -0600

    ran prettier

--- new/src/components/pages/Records.jsx
+++ new/src/components/pages/Records.jsx
@@ -5,34 +5,44 @@ import { connect } from "react-redux";
 import { teamMembers, battle } from "../../store/actions";
 
 const Records = props => {
-	const { getPokemonByTeam, getBattleData, pokemonInTeam, teamName, wins, losses, ties, battleAverage, bestScore } = props;
+	const {
+		getPokemonByTeam,
+		getBattleData,
+		pokemonInTeam,
+		teamName,
+		wins,
+		losses,
+		ties,
+		battleAverage,
+		bestScore,
+	} = props;
 	const params = useParams();
 	useEffect(() => {
-        getPokemonByTeam(params.teamId);
-        getBattleData(params.teamId);
+		getPokemonByTeam(params.teamId);
+		getBattleData(params.teamId);
 	}, [getPokemonByTeam, getBattleData]);
 	return (
 		<div>
 			<header>
-                <h2>Records Room</h2>
-                <h3>{teamName}</h3>
+				<h2>Records Room</h2>
+				<h3>{teamName}</h3>
 			</header>
-            <section>
-            <h3>Wins: {wins}</h3>
-            <h3>Losses: {losses}</h3>
-            <h3>Ties: {ties}</h3>
-            <h3>Win/Loss Ratio: {isNaN(wins / losses) ? 0 : wins / losses}</h3>
-            <h3>Average Score: {isNaN(battleAverage) ? 0 : battleAverage}</h3>
-            <h3>Best Score: {bestScore === -Infinity ? 0 : bestScore}</h3>
-            {pokemonInTeam.map(pokemon => {
-                return (
-                    <div key={pokemon.pokemon_Id}>
-                    <img src={pokemon.imgURL} alt={pokemon.name} />
-                    <h3>{pokemon.nickname ? pokemon.nickname : pokemon.name}</h3>
-                    </div>
-                );
-            })}
-            </section>
+			<section>
+				<h3>Wins: {wins}</h3>
+				<h3>Losses: {losses}</h3>
+				<h3>Ties: {ties}</h3>
+				<h3>Win/Loss Ratio: {isNaN(wins / losses) ? 0 : wins / losses}</h3>
+				<h3>Average Score: {isNaN(battleAverage) ? 0 : battleAverage}</h3>
+				<h3>Best Score: {bestScore === -Infinity ? 0 : bestScore}</h3>
+				{pokemonInTeam.map(pokemon => {
+					return (
+						<div key={pokemon.pokemon_Id}>
+							<img src={pokemon.imgURL} alt={pokemon.name} />
+							<h3>{pokemon.nickname ? pokemon.nickname : pokemon.name}</h3>
+						</div>
+					);
+				})}
+			</section>
 			<footer></footer>
 		</div>
 	);
@@ -40,28 +50,28 @@ const Records = props => {
 
 Records.propTypes = {
 	getPokemonByTeam: PropTypes.func,
-    pokemonInTeam: PropTypes.array,
-    teamName: PropTypes.string,
-    getBattleData: PropTypes.func,
-    wins: PropTypes.number,
-    losses: PropTypes.number,
-    ties: PropTypes.number,
-    battleAverage: PropTypes.number,
-    bestScore: PropTypes.number
+	pokemonInTeam: PropTypes.array,
+	teamName: PropTypes.string,
+	getBattleData: PropTypes.func,
+	wins: PropTypes.number,
+	losses: PropTypes.number,
+	ties: PropTypes.number,
+	battleAverage: PropTypes.number,
+	bestScore: PropTypes.number,
 };
 
 export default connect(
 	state => ({
-        pokemonInTeam: state.teamMembers.pokemonInTeam,
-        teamName: state.teams.teamName,
-        wins: state.battle.wins,
-        losses: state.battle.losses,
-        ties: state.battle.ties,
-        battleAverage: state.battle.battleAverage,
-        bestScore: state.battle.bestScore
+		pokemonInTeam: state.teamMembers.pokemonInTeam,
+		teamName: state.teams.teamName,
+		wins: state.battle.wins,
+		losses: state.battle.losses,
+		ties: state.battle.ties,
+		battleAverage: state.battle.battleAverage,
+		bestScore: state.battle.bestScore,
 	}),
 	{
-        getPokemonByTeam: teamMembers.getPokemonByTeam,
-        getBattleData: battle.getBattleData
+		getPokemonByTeam: teamMembers.getPokemonByTeam,
+		getBattleData: battle.getBattleData,
 	}
 )(Records);
