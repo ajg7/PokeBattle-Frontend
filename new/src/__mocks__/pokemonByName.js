d784ea1d885b440f2b7adc78a688a8cc2e1f0152
ajg7 <ajgebara@gmail.com>
2021-01-28 13:11:29 -0600

    made pokemon api mock calls

---
+++ new/src/__mocks__/pokemonByName.js
@@ -0,0 +1,24 @@
+export const pokemonByName = [
+	{
+		id: 25,
+		name: "pikachu",
+		type1: "electric",
+		type2: null,
+		imgURL:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/25.png",
+		number: 25,
+		height: 4,
+		weight: 60,
+		entry:
+			"When several of these POKéMON gather, their electricity could build and cause lightning storms.",
+		habitat: "forest",
+		legendary: false,
+		mythical: false,
+		ancient: false,
+		region: "kanto",
+		modern_imgURL:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png",
+		shiny_imgURL:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/25.png",
+	},
+];
--- new/src/__tests__/pokemonAPICalls.test.js
+++ new/src/__tests__/pokemonAPICalls.test.js
@@ -1,14 +1,7 @@
 // import mockedAxios from "axios";
 import { pokemon } from "../__mocks__/pokemon";
+import { pokemonByName } from "../__mocks__/pokemonByName";
 import axios from "axios";
-
-/*
-
-Goal: 
-    -Need to test that Pokemon API call returns a list of all pokemon
-    -Test that when you query the list by name it returns that specific pokemon
-    -Test that featured Pokemon is a random pokemon from the list
-*/
 
 jest.mock("axios");
 
@@ -20,5 +13,19 @@ describe("Pokemon API Calls", () => {
 		const result = await axios.get("http://localhost:7000/pokemon");
 		expect(result).toEqual(pokemon);
 	});
-	test("Get Pokemon By Name", async () => {});
+	test("Get Pokemon By Name", async () => {
+		const response = pokemonByName;
+		axios.get.mockResolvedValue(response);
+		const result = await axios.get(
+			`http://localhost:7000/pokemon/?search=${pokemonByName[0].name}`
+		);
+		expect(result).toEqual(pokemonByName);
+	});
+	test("Random Pokemon from List", async () => {
+		const response = pokemon;
+		axios.get.mockResolvedValue(response);
+		const result = await axios.get("http://localhost:7000/pokemon");
+		const randomMember = result[Math.round(Math.random() * 151)];
+		expect(randomMember).toBeDefined;
+	});
 });
