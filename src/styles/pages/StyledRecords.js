31707d3eca6277724241b0314464026a73573f98
ajg7 <ajgebara@gmail.com>
2021-02-03 10:39:18 -0600

    records mobile done

--- src/styles/pages/StyledRecords.js
+++ src/styles/pages/StyledRecords.js
@@ -4,6 +4,12 @@ export const StyledRecords = styled.div`
 	padding: ${({ theme }) => theme.globalPadding};
 	text-transform: ${({ theme }) => theme.cap};
 
+	@media (max-width: 450px) {
+		h3 {
+			margin: 10px;
+		}
+	}
+
 	.team {
 		display: flex;
 		flex-flow: row wrap;
@@ -23,6 +29,7 @@ export const StyledRecords = styled.div`
 
 	header {
 		display: flex;
+		flex-flow: row wrap;
 		justify-content: space-evenly;
 	}
 
