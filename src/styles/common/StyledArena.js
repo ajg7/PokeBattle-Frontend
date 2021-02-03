990ad75210219c1dc25a6739db2ad9943ae20111
ajg7 <ajgebara@gmail.com>
2021-02-02 19:51:55 -0600

    added color conditional in styled arena

--- src/styles/common/StyledArena.js
+++ src/styles/common/StyledArena.js
@@ -20,49 +20,23 @@ export const StyledArena = styled.div`
 	h3 {
 		margin: 20px 0;
 		text-align: center;
+		color: ${({ backgroundColor }) => {
+			switch (backgroundColor) {
+				case "#0040FF":
+					return "white";
+				case "#40826D":
+					return "white";
+				case "#ca2c92":
+					return "white";
+				default:
+					return "black";
+			}
+		}};
 	}
 
 	img {
 		margin: 15px 0;
-		height: 150px;
-		width: 150px;
+		height: 200px;
+		width: 200px;
 	}
-
-	/* display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	height: 100%;
-	width: 800px;
-	background-color: #028a0f;
-	margin: 25px 0;
-
-	h3 {
-		padding: 25px 0;
-		text-align: center;
-		font-size: 40px;
-	}
-
-	img {
-		height: 100px;
-		width: 100px;
-	}
-
-	.player-team-slot,
-	.challenger-team-slot {
-		height: 100px;
-		width: 100px;
-		border: 5px dotted blue;
-		text-align: center;
-		margin: 25px 0;
-	}
-
-	.battle {
-		display: flex;
-		flex-flow: column wrap;
-	}
-
-	.battle img {
-		margin: 20px 0;
-	} */
 `;
