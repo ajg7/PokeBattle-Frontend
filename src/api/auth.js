01ecc799113dd8ec95f4371be1a6544f8a7645b9
ajg7 <ajgebara@gmail.com>
2021-02-05 22:36:13 -0600

    added heroku link to the auth functions as well:

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 export const signUp = async user => {
-	const data = await axios.post("http://localhost:7000/users/signup", user);
+	const data = await axios.post("https://pokebattle-backend.herokuapp.com/users/signup", user);
 	localStorage.setItem("token", data.data.token);
 	localStorage.setItem("userId", data.data.userId);
 	if (!localStorage.getItem("token")) throw new Error();
 };
 
 export const login = async user => {
-	const data = await axios.post("http://localhost:7000/users/login", user);
+	const data = await axios.post("https://pokebattle-backend.herokuapp.com/users/login", user);
 	localStorage.setItem("token", data.data.token);
 	localStorage.setItem("userId", data.data.userId);
 	if (!localStorage.getItem("token")) throw new Error();
--- src/store/actions/whosThatPokemonActions.js
+++ src/store/actions/whosThatPokemonActions.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 export const GET_TOTAL_POINTS = "GET_TOTAL_POINTS";
 export const getTotalPoints = id => async dispatch => {
-	const { data } = await axios.get(`http://localhost:7000/users/points/${id}`);
+	const { data } = await axios.get(`https://pokebattle-backend.herokuapp.com/users/points/${id}`);
 	dispatch({ type: GET_TOTAL_POINTS, payload: data.total_points });
 };
 
 export const UPDATE_TOTAL_POINTS = "UPDATE_TOTAL_POINTS";
 export const updateTotalPoints = (updatedPoints, id) => async dispatch => {
-	await axios.put(`http://localhost:7000/users/points/${id}`, updatedPoints);
+	await axios.put(`https://pokebattle-backend.herokuapp.com/users/points/${id}`, updatedPoints);
 	dispatch({ type: UPDATE_TOTAL_POINTS });
 };
